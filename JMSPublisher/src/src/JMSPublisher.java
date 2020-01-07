528ddc55a0cf676bf23f76138ecc0241853ae4d7
e.n.dutkiewicz@gmail.com <e.n.dutkiewicz@gmail.com>
2020-01-07 21:56:32 +0100

    ...

--- JMSPublisher/src/src/JMSPublisher.java
+++ JMSPublisher/src/src/JMSPublisher.java
@@ -1,32 +1,27 @@
 package src;
+
 import javax.jms.ConnectionFactory;
 import javax.jms.JMSContext;
 import javax.jms.JMSException;
 import javax.jms.JMSProducer;
-import javax.jms.Queue;
+import javax.jms.Topic;
 
-public class Producer {
-	
-	private JMSContext jmsContext;
-	private JMSProducer jmsProducer;
-	private Queue queue;
-	Producer(String url, String queueName) throws JMSException {
-	ConnectionFactory connectionFactory = new com.sun.messaging.ConnectionFactory();
-	// [hostName][:portNumber][/serviceName]
-	// 7676 numer portu, na którym JMS Service nas³uchuje po³¹czeñ
-	// "localhost:7676/jms"
-	jmsContext = connectionFactory.createContext();
-	((com.sun.messaging.ConnectionFactory)connectionFactory)
-	.setProperty(com.sun.messaging.ConnectionConfiguration.imqAddressList, url);
-	jmsProducer = jmsContext.createProducer();
-	queue = new com.sun.messaging.Queue(queueName); // "ATJQueue"
-	}
-	public void sendQueueMessage(String msg) {
-	jmsProducer.send(queue, msg);
-	}
-	protected void finalize() {
-	if (jmsContext != null)
-	jmsContext.close();
+public class JMSPublisher {
+	public void publishTopicMessages() {
+		ConnectionFactory connectionFactory = new com.sun.messaging.ConnectionFactory();
+		JMSContext jmsContext = connectionFactory.createContext();
+		JMSProducer jmsProducer = jmsContext.createProducer();
+		try {
+			Topic topic = new com.sun.messaging.Topic("ATJTopic");
+			for (int i = 0; i < 10; i++) {
+				String msg = "Message_" + i;
+				jmsProducer.send(topic, msg);
+				System.out.printf("Wiadomoœæ '%s' zosta³a opublikowana.\n", msg);
+			}
+		} catch (JMSException e) {
+			e.printStackTrace();
+		}
+		jmsContext.close();
 	}
 
 }
