f997e4e5721f7beccfba55a29afd82d6810430ec
e.n.dutkiewicz@gmail.com <e.n.dutkiewicz@gmail.com>
2020-01-07 21:14:24 +0100

    create classes Producer and Main

---
+++ JMSProducer/src/src/Main.java
@@ -0,0 +1,18 @@
+//P2P
+package src;
+
+import javax.jms.JMSException;
+
+public class Main {
+
+	public static void main(String[] args) throws JMSException {
+		Producer producer = new Producer("localhost:7676/jms", "ATJQueue");
+		producer.sendQueueMessage("m1");
+		producer.sendQueueMessage("m2");
+		producer.sendQueueMessage("m3");
+		producer.sendQueueMessage("m4");
+		producer.sendQueueMessage("m5");
+		producer.finalize();
+	}
+
+}
